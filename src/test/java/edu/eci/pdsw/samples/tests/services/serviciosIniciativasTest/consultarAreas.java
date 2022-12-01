372a2644fd2872e0e134a67e1656104a7162188e
unknown <julia.mejia@labinfo.is.escuelaing.edu.co>
2022-12-01 15:04:09 -0500

    Test Cantidad Areas

--- src/test/java/edu/eci/pdsw/samples/tests/services/serviciosIniciativasTest/consultarAreas.java
+++ src/test/java/edu/eci/pdsw/samples/tests/services/serviciosIniciativasTest/consultarAreas.java
@@ -43,31 +43,6 @@ public class consultarAreas {
     }
 
     @Test
-    public void dadoUnAreaSeDebePoderObtenerElNumeroDeAreas() throws SQLException, ExcepcionServiciosSuscripciones {
-        //Insertar datos en la base de datos de pruebas, de acuerdo con la clase
-        //de equivalencia correspondiente
-
-        // ARRANGE
-        clearDB();
-        Connection conn=getConnection();
-        Statement stmt=conn.createStatement();
-        stmt.execute("INSERT INTO areas VALUES (1,'Estudiantes PreGrado')");
-        stmt.execute("INSERT INTO areas VALUES (2,'Profesores')");
-        stmt.execute("INSERT INTO `usuarios` (`idUsuario`,`perfil`,`informacion`,`correo`,`contrasena`) VALUES (1,'fulanito','islazy','veganito2000@mail.com','veganito')");
-        stmt.execute("INSERT INTO `usuarios` (`idUsuario`,`perfil`,`informacion`,`correo`,`contrasena`) VALUES (2,'pepe','islazy','pepe2004@mail.com','pepe')");
-        stmt.execute("INSERT INTO `iniciativas` (`estado`,`descripcion`,`fecha`,`usuarios_idUsuario`,`areas_idArea`) VALUES ('en proceso','su solicitud esta siedo revisada','2008-7-04',1,1)");
-        stmt.execute("INSERT INTO `iniciativas` (`estado`,`descripcion`,`fecha`,`usuarios_idUsuario`,`areas_idArea`) VALUES ('rechazada','su solicitud esta siedo revisada','2008-7-04',2,2)");
-
-        conn.close();
-
-        //Realizar la operacion de la logica y la prueba
-        List <Area> areas = ServiciosIniciativasFactory.getInstance().getTestingForumServices().consultarCantidadAreas();
-        //assert ...
-        Assert.assertEquals(areas.size(),2);
-
-    }
-
-    @Test
     public void dadosUnosDatosEnLaBDSeDebePoderAveriguarQueAreaEs() throws SQLException, ExcepcionServiciosSuscripciones {
         //Insertar datos en la base de datos de pruebas, de acuerdo con la clase
         //de equivalencia correspondiente
